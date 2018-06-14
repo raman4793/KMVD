bec87d7080bd2d4c4224430520b7a55ce29bbbf8
ram <raman4793@gmail.com>
2018-06-14 12:49:20 +0530

    fixed conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
+
   resources :parkings
   resources :spots
   resources :types
-=======
->>>>>>> 298d345999cccd116452ea795bb154ecabdc7090
   devise_for :users
   resources :vehicle_fines
   resources :fines
